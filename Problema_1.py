0ac7d3dcbbda23d93089497f0fae3d6a255254ad
AkameKM <167798143+AkameKM@users.noreply.github.com>
2024-09-27 01:41:19 +0000

    Problem_1

--- Problema_1.py
+++ Problema_1.py
@@ -1,9 +1,6 @@
 
-"""
-Problema 1:
-Escribe un programa en Python para encontrar los números que son divisibles por 7 y múltiplos de 5,
-en el rango de 1500 y 2700 (ambos incluidos). 
-"""
+
+############# Problema 1 ############
 
 
 def numeros():
