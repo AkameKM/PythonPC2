20a4ff506c09d94582535d91ee764e5ae992d17b
AkameKM <167798143+AkameKM@users.noreply.github.com>
2024-09-28 18:15:54 +0000

    Problema_5

--- Problema_5.py
+++ Problema_5.py
@@ -1,2 +1,18 @@
 ###################### Problema 5 #########################
-#def num_primos(n):
+def num_primo(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def sum_primos_menores_100():
+    suma = 0
+    for num in range(2, 100):
+        if num_primo(num):
+            suma += num
+    return suma
+
+resultado = sum_primos_menores_100()
+print(f"La suma de los números primos menores que 100 es: {resultado}")
